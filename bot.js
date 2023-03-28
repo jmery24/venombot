0286d79a67c26dadacf654645530c526b95e8e3a
Juan Manuel Mery <jmery24@gmail.com>
2023-03-28 14:15:21 -0300

    2 commit

--- bot.js
+++ bot.js
@@ -1,26 +1,19 @@
 const venom = require('venom-bot');
 const fs = require('fs');
-
-const dbPath = 'database.json';
-const db = fs.existsSync(dbPath) ? require(dbPath) : {};
-
 const sqlite3 = require('sqlite3').verbose();
 
 // Abrir una conexión con la base de datos
-let db2 = new sqlite3.Database('partidos.db');
+let db = new sqlite3.Database('botvenom.db');
 
-// Crear la tabla de partidos si no existe
-db2.run('CREATE TABLE IF NOT EXISTS partidos (id INTEGER PRIMARY KEY, fecha TEXT, lugar TEXT, hora TEXT, cantjugadores INTEGER)');
 
-db2.all('SELECT * FROM partidos', [], (err, rows) => {
-    if (err) {
-     throw err;
-}
-        rows.forEach((row) => {
-    console.log(row.id, row.fecha, row.lugar, row.hora, row.cantjugadores);
-    
-});
-    db2.close();
+// Crear la tablas de las base de datos si no existen
+
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS jugadores (idjugador INTEGER PRIMARY KEY, nombre TEXT, apellido TEXT, telefono TEXT, idwhatsapp TEXT, club TEXT, fechaNac TEXT, arquero TEXT, defensa TEXT, delantero TEXT, titular TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS partidos (idpartido INTEGER PRIMARY KEY, fecha TEXT, hora TEXT, lugar TEXT, cantjugadores INTEGER)');
+  db.run('CREATE TABLE IF NOT EXISTS equipos (idequipo INTEGER PRIMARY KEY, idpartido INTEGER, equipo TEXT, idjugador1 INTEGER, idjugador2 INTEGER, idjugador3 INTEGER, idjugador4 INTEGER, idjugador5 INTEGER, idjugador6 INTEGER, idjugador7 INTEGER, idjugador8 INTEGER, idjugador9 INTEGER, idjugador10 INTEGER, idjugador11 INTEGER)');
+  db.run('CREATE TABLE IF NOT EXISTS resultados (idresultado INTEGER PRIMARY KEY, idpartido INTEGER, idequipo INTEGER, goles INTEGER)');
+  db.run('CREATE TABLE IF NOT EXISTS configuracion (idconfig INTEGER PRIMARY KEY, dia TEXT, hora TEXT, lugar TEXT, cantjugadores INTEGER, equipoA TEXT, equipoB TEXT, capitanTel TEXT)');
 });
 
 
@@ -29,53 +22,38 @@ venom
   .then((client) => {
     client.onMessage((message) => {
       if (message.body.startsWith('/partido')) {
-        // Parse the message to get the match details.
-        const match = parseMatchDetails(message.body);
+        const msgPartido = parsePartido(message.body);
         console.log(message.body);
-        console.log(match);
+        console.log(partido);
         console.log(message.sender.id);
         console.log(message.from);
 
-        let fecha = match.date
-        let hora = match.time
-        let lugar = match.location
-        let cantjugadores = match.players
-        db2.run('INSERT INTO partidos (fecha, lugar, hora, cantjugadores) VALUES (?, ?, ?, ?)', [fecha, lugar, hora, cantjugadores], function(err) {
+        let fecha = msgPartido.msgFecha
+        let hora = msgPartido.msgHora
+        let lugar = msgPartido.msgLugar
+        let cantJugadores = msgPartido.msgCantJugadores
+        db.run('INSERT INTO partidos (fecha, lugar, hora, cantjugadores) VALUES (?, ?, ?, ?)', [fecha, lugar, hora, cantugadores], function(err) {
             if (err) {
               return console.error(err.message);
             }
-            console.log(`Se ha insertado el partido el ${fecha} ${hora} en ${lugar} de ${cantjugadores} jugadores`);
-            client.sendText(message.from, `Partido creado: ${match.date}, ${match.location}, ${match.time}, ${match.players} jugadores.`);
+            console.log(`Se ha insertado el partido el ${fecha} ${hora} en ${lugar} de ${cantJugadores} jugadores`);
           });
-          
-          db2.close();
+          client.sendText(message.from, `Partido creado: ${msgPartido.msgFecha}, ${msgPartido.msgLugar}, ${msgPartido.msgHora}, ${msgPartido.msgCantJugadores} jugadores.`);
+          db.close();
         };
-
-
-        // Store the match in the database.
-        //db.matches.push(match);
-        //saveDatabase();
-       
     });
   })
   .catch((error) => {
     console.log(error);
   });
 
-function parseMatchDetails(message) {
-  // Parse the message body to get the match details.
+
+// funciones de parseo de los mensajes de whatsapp 
+function parsePartido(message) {
   const parts = message.split(' ');
-  const date = parts[1];
-  const location = parts[2];
-  const time = parts[3];
-  const players = parseInt(parts[4]);
-  return { date, location, time, players, playersConfirmed: [] };
-}
-
-function saveDatabase() {
-  // Save the database to disk.
-  fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
- };
-
-
-
+  const msgFecha = parts[1];
+  const msgLugar = parts[2];
+  const msgHora = parts[3];
+  const msgCantJugadores = parseInt(parts[4]);
+  return { msgFecha, msgLugar, msgHora, msgCantJugadores};
+};
